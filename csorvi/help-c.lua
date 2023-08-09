770e98eb6c0ea10db56fd2ece0f7d65f056c97bc
csorvi <csorvi06@gmail.com>
2023-08-09 22:39:40 +0200

    asd

--- csorvi/help-c.lua
+++ csorvi/help-c.lua
@@ -4,16 +4,19 @@
 -- This function is used to register a command for the chat. 
 --When this is exectuted, it will execute a function, doing the msg function to the client
 RegisterCommand('gun', function()
-    GiveWeaponToPed(GetPlayerPed(-1), 1305664598 , 1000, false, true)
+    GiveWeaponToPed(GetPlayerPed(-1), -1568386805 , 1000, false, true)
     msg("fasz")
 end, false)
 
 
 RegisterCommand('csorvi', function()
-	
+
         StopResource(csorvi)
         StartResource(csorvi)
         msg("a szar ujra indul")
 end, false)
 
 
+function msg(text)
+    TriggerEvent("chatMessage",  "[Server]", {255,0,0}, text)
+end 
