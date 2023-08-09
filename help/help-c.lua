add85febf9d223d6c12b83594dfd8abe990a67f0
csorvi <csorvi06@gmail.com>
2023-08-09 21:58:06 +0200

    asd

---
+++ help/help-c.lua
@@ -0,0 +1,15 @@
+--[[ help script made at JEVA by Zua (https://github.com/thatziv) ]]
+
+
+-- This function is used to register a command for the chat. 
+--When this is exectuted, it will execute a function, doing the msg function to the client
+RegisterCommand('help', function()
+    msg("Discord: discord.gg/ftZn3Q6C")
+    msg("Website: website.com")
+    msg("teamspeak: website.com")
+end, false)
+
+
+RegisterCommand('help', function()
+    msg("kurva anyad")
+end, false)
