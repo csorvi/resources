f7fb97f46f33af5ea935a0d52af1bc8363c9a19d
peternagy04 <peternagy503@gmail.com>
2023-08-09 22:00:33 +0200

    fas

---
+++ deadgaming/client.lua
@@ -0,0 +1,23 @@
+--[[ help script made at JEVA by Zua (https://github.com/thatziv) ]]
+
+
+-- This function is used to register a command for the chat. 
+--When this is exectuted, it will execute a function, doing the msg function to the client
+RegisterCommand('help', function()
+    msg("Discord: discord.gg/ftZn3Q6C")
+    msg("Website: website.com")
+    msg("teamspeak: website.com")
+end, false)
+
+
+RegisterCommand('help', function()
+    msg("kurva anyad")
+end, false)
+
+function spawnCar(car)
+    local car=GetHashKey(car)
+
+    RequestModel(car)
+    while not HasModelLoaded(car) do
+        RequestModel(car)
+        Citizen
