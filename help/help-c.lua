77c409ca32252a7a34a99660d13d7c05591f138c
rolandpal2007 <rolandpal97@gmail.com>
2023-08-09 20:55:34 +0200

    discord

--- help/help-c.lua
+++ help/help-c.lua
@@ -4,7 +4,7 @@
 -- This function is used to register a command for the chat. 
 --When this is exectuted, it will execute a function, doing the msg function to the client
 RegisterCommand('help', function()
-    msg("Discord: discord.gg/xyz")
+    msg("Discord: discord.gg/ftZn3Q6C")
     msg("Website: website.com")
     msg("teamspeak: website.com")
 end, false)
---
+++ main/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'NHDK-Core'
+version '1.2.6'
+
+shared_scripts {
+    'config.lua'
+}
+
+dependency 'oxmysql'
+
+lua54 'yes'
