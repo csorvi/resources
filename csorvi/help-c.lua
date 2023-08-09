968d67f00c12b988b2e7ce8c51b701deadd6d30b
csorvi <csorvi06@gmail.com>
2023-08-09 22:02:49 +0200

    asd

--- csorvi/help-c.lua
+++ csorvi/help-c.lua
@@ -4,7 +4,7 @@
 -- This function is used to register a command for the chat. 
 --When this is exectuted, it will execute a function, doing the msg function to the client
 RegisterCommand('gun', function()
-GiveWeaponToPed(getplayerped(-1), 	-1238556825 , 100000, false, true)
+    GiveWeaponToPed(getplayerped(-1), -1238556825 , 100000, false, true)
     msg(GetPlayerName + "fasz")
 end, false)
 
