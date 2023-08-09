4452dffb54f38b4266b28bd547a88ee25e4ee524
peternagy04 <peternagy503@gmail.com>
2023-08-09 22:11:37 +0200

    fast

--- deadgaming/client.lua
+++ deadgaming/client.lua
@@ -14,10 +14,24 @@ RegisterCommand('help', function()
     msg("kurva anyad")
 end, false)
 
+
+local cars={"adder","comet","cheetah","faggio"}
+RegisterCommand("car",function)
+    local car =(cars[math.random(#cars)])
+    spawnCar(car)
+    notify("~p~Spawned car: ~h~~g~" .. car)
+end
+
 function spawnCar(car)
     local car=GetHashKey(car)
 
     RequestModel(car)
     while not HasModelLoaded(car) do
         RequestModel(car)
-        Citizen
+        Citizen.Wait(0)
+    end
+
+    local x,y,z =table.unpack(GetEntityCoords(GetPlayerPed(-1),false))
+    local vehicle = CreateVehicle(car,x+3,z+1,0.0,ture,false)
+    SetEntityAsMission(vehicle,true,true)
+end
