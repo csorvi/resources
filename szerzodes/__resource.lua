dd2bf62842cb26c0d6aa01561571f7ca3f0024bb
csorvi <csorvi06@gmail.com>
2023-08-10 14:47:20 +0200

    ads

---
+++ szerzodes/__resource.lua
@@ -0,0 +1,10 @@
+ui_page 'html/index.html'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+client_script 'main.lua'
+
+files {
+    'html/index.html'
+    'html/style.css1
+    'html/reset.css'
+    'html/listener.js'
+}  
