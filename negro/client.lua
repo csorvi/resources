51e6d927bcf1383942388a000b622d0e19f7f1f4
rolandpal2007 <rolandpal97@gmail.com>
2023-08-10 15:22:36 +0200

    ped

--- negro/client.lua
+++ negro/client.lua
@@ -1,6 +1,10 @@
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        NetworkSetFriendlyFireOption(true)
-    end
-end)
+local function CreateDutyBlips(playerId, playerLabel, playerJob, playerLocation)
+    local ped = GetPlayerPed(-1)
+    msg(ped)
+end
+
+CreateDutyBlips()
+
+function msg(text)
+    TriggerEvent("chatMessage",  "[Server]", {255,0,0}, text)
+end 
